0d674e96495915eb2ca3edbe6f17ade5de0d5ff8
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2021-10-21 17:46:57 -0400

    Update CopyAndMoveSemanticsBasics.cpp

--- Resources/C++/CopyAndMoveSemanticsBasics.cpp
+++ Resources/C++/CopyAndMoveSemanticsBasics.cpp
@@ -11,15 +11,15 @@ public:
 	MyClass(const MyClass& myClass);
 
 	// Move Constructor: notice how it's not "const" because it will be changed
-	MyClass(MyClass&& myClass) noexcept; // "noexcept" just means it may not throw exceptions
-										 // move constructors should have to have this keyword
+	MyClass(MyClass&& myClass) noexcept; 			// "noexcept" just means it may not throw exceptions
+					     			// move constructors should have to have this keyword
 										 
 	// Copy assignment
 	MyClass& operator=(const MyClass& myClass);
 
 	// Move assignment: notice how it's not "const" because it will be changed
-	MyClass& operator=(MyClass&& myClass) noexcept; // once again, should not throw exceptions
-													// move assignment operators should have this keyword
+	MyClass& operator=(MyClass&& myClass) noexcept; 	// once again, should not throw exceptions
+								// move assignment operators should have this keyword
 
 	~MyClass() { delete[] a; }
 
@@ -32,29 +32,29 @@ int main() {
 
 	// CONSTRUCTOR
 	MyClass mc1;
-	MyClass mc2(mc1);	// copy constructor called. mc1 is an lvalue
+	MyClass mc2(mc1);		// copy constructor called. mc1 is an lvalue
 
 	cout << endl << endl;
 
 	MyClass mc3;
-	MyClass mc4(move(mc3)); // move constructor called. move() converts the lvalue mc3 to an rvalue
+	MyClass mc4(move(mc3)); 	// move constructor called. move() converts the lvalue mc3 to an rvalue
 
 	cout << endl << endl;
 
 	// ASSIGNMENT OPERATOR
 	MyClass mc6;
 	MyClass mc7;
-	mc7 = mc6; // copy assignment called. mc6 is an lvalue
+	mc7 = mc6; 			// copy assignment called. mc6 is an lvalue
 
 	cout << endl << endl;
 
 	MyClass mc8;
 	MyClass mc9;
-	mc9 = move(mc8); // move assignment called. move() converts the lvalue mc8 to an ravlue
+	mc9 = move(mc8); 		// move assignment called. move() converts the lvalue mc8 to an ravlue
 	cout << endl;
 	MyClass mc10;
 	cout << endl;
-	mc10 = MyClass(); // move assignment called. MyClass() is an rvalue
+	mc10 = MyClass(); 		// move assignment called. MyClass() is an rvalue
 	cout << endl;
 
 	return 0;
