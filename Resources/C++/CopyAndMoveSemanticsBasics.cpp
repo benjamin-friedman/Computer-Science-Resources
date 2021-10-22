c7a0f283a68bd04b9a22c239937bb92d5979c1d9
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2021-10-22 14:34:13 -0400

    Update CopyAndMoveSemanticsBasics.cpp

--- Resources/C++/CopyAndMoveSemanticsBasics.cpp
+++ Resources/C++/CopyAndMoveSemanticsBasics.cpp
@@ -1,3 +1,30 @@
+/*
+   This program demonstrates the basics of C++ copy and move semantics:
+   	- copy assignment/copy constructor
+	- move assignment/move constructor
+	- the "std::move()" function
+	- lvalues vs. rvalues (brief description below)
+	
+	
+   Though lvalues and rvalues have official definitions, the best way to think of them in general is as follows:
+   An lvalue is something you can reference by a name, an rvalue is something that you cannot reference by a name.
+   Meaning, in "int x = 3;", x is an lvalue since it's a variable with a name, whereas 3 is an rvalue since it's just
+   the constant 3. It is not stored in a variable, and after the statement is executed it's gone. Note that you CAN give an
+   rvalue a name, but this is for special cases like move semantics where an rvalue is a formal parameter in a function.
+   A common misconception about lvalues and rvalues is that the l and r stand for "left" and "right" meaning lvalues go
+   on the left side of the "=" operator, and r values go on the right side. As you will see in the examples below, this
+   is not the case.
+   
+   int x = 3;				x is an lvalue, 3 is an rvalue
+   int y = x;				x and y are both lvalues
+   string s = "Frederic Chopin";	s is an lvalue, "Frederic Chopin" is an rvalue
+   string s2 = s;			s2 and s are both lvalues
+   
+   Can an rvalue go on the left of the "=" operator? Yes.
+   s + s = s;				"s + s" is an rvalue, s is an lvalue
+   Though this statement doesn't do anything of significance, it is valid C++ code.
+*/
+
 #include <iostream>
 
 using namespace std;
