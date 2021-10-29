75b4a5f652b08d88fffabe6435292680ab65bf12
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2021-10-29 18:45:25 -0400

    Update ExampleClass.cpp

--- Resources/C++/01 - Class Basics/ExampleClass.cpp
+++ Resources/C++/01 - Class Basics/ExampleClass.cpp
@@ -7,40 +7,34 @@ const double ExampleClass::PI = 3.14159;
 
 /***** Constructors ******/
 /* Default Constructor - will initialize all of the member variables to default values.
-   Can be initialized in the body, or using the special notation after the colon. */
-ExampleClass::ExampleClass() : str("Ballad 1 in G Minor"), x(1849) {
-	size = 0;
-	capacity = 10;
-	vec = new int[capacity];
+   Can be initialized in the body, or using the member initializer list. */
+ExampleClass::ExampleClass() : str("Ballad 1 in G Minor"), x(1849), size(0), capacity(3) {
+	a = new int[capacity];
+	cout << "Default constructor called for object at address " << this << endl;
 }
 
 
 /* 1 parameter constructor, should use explicit */
-ExampleClass::ExampleClass(int _capacity) : str("Ballad 1 in G Minor"), x(1849),
-capacity(_capacity)
-{
-	vec = new int[capacity];
-	size = 0;
+ExampleClass::ExampleClass(int _capacity) : str("Ballad 1 in G Minor"), x(1849), size(0), capacity(_capacity) {
+	a = new int[capacity];
+	cout << "1-Parameter constructor called for the object at address " << this << endl;
 }
 
 
 /* 3 arg constructor - will initialize all of the member variables to either one of the 3 arguemnts
    provided or default values. */
-ExampleClass::ExampleClass(string _str, double _x, int _capacity) : str(_str), x(_x), size(0) {
-	capacity = (_capacity);
-	vec = new int[capacity];
+ExampleClass::ExampleClass(string _str, double _x, int _capacity) : str(_str), x(_x), size(0), capacity(_capacity) {
+	a = new int[capacity];
+	cout << "3-Parameter constructor called for the object at address " << this << endl;
 }
 
 
 /* Copy Constructor - constructs a new object that is a complete and independent copy of "ec". */
-ExampleClass::ExampleClass(const ExampleClass& ec) {
-	str = ec.str;
-	x = ec.x;
-	size = ec.size;
-	capacity = ec.capacity;
-	vec = new int[capacity];
+ExampleClass::ExampleClass(const ExampleClass& ec) : str(ec.str), x(ec.x), size(ec.size), capacity(ec.capacity) {
+	a = new int[capacity];
 	for (int i = 0; i < size; i++)
-		vec[i] = ec.vec[i];
+		a[i] = ec.a[i];
+	cout << "Copy constructor called for the object at address " << this << endl;
 }
 
 
@@ -48,7 +42,8 @@ ExampleClass::ExampleClass(const ExampleClass& ec) {
 
 /***** Destructor *****/
 ExampleClass::~ExampleClass() {
-	delete[] vec;		// vec was allocated on the heap with "new"
+	delete[] a;		// vec was allocated on the heap with "new"
+	cout << "Destructor called for object at address " << this << endl;
 }
 
 
@@ -56,20 +51,25 @@ ExampleClass::~ExampleClass() {
 
 /* Copy Assignment Operator */
 ExampleClass& ExampleClass::operator=(const ExampleClass& ec) {
-	if (this == &ec)		// prevent from copying to self
+	if (this == &ec)			// prevent from copying to self
 		return *this;
 
+	/* Copy the values from ec to the calling object */
 	str = ec.str;
 	x = ec.x;
 	size = ec.size;
 	capacity = ec.capacity;
-
-	if (vec != nullptr)			// free up the current array before creating the new array
-		delete[] vec;			// not doing this would cause a memory leak
-	vec = new int[capacity];
+			
+	/* Make a deep copy of the array in ec, and store it in the calling object. Simply
+	   writing the line
+	        a = ec.a
+	   would mean "a" and "ec.a" share the same array instead of "a" having a copy of the array. */
+	delete[] a;					
+	a = new int[capacity];
 	for (int i = 0; i < size; i++)
-		vec[i] = ec.vec[i];
+		a[i] = ec.a[i];
 
+	cout << "Copy assignment operator called for object at address " << this;
 	return *this;
 }
 
@@ -93,8 +93,8 @@ int ExampleClass::getCapacity() const {
 
 
 int ExampleClass::getIntegerAtIndex(int index) const {
-	if (index > 0 && index < size)
-		return vec[index];
+	if (index >= 0 && index < size)
+		return a[index];
 	else {
 		cout << "Cannot retrieve integer at index " << index << " - it is an invalid index." << endl << endl;
 		return -9090;
@@ -117,14 +117,9 @@ void ExampleClass::ExampleClass::setX(double newX) {
 
 void ExampleClass::ExampleClass::pushBack(int newItem) {
 	if (size == capacity) {
-		int* newVec = new int[capacity * 2];
-		for (int i = 0; i < capacity; i++)
-			newVec[i] = vec[i];
-		delete[] vec;
-		vec = newVec;
-		capacity *= 2;
+		resizeArray();		// call the helper function to resize the array.
 	}
-	vec[size++] = newItem;
+	a[size++] = newItem;
 }
 
 
@@ -138,7 +133,7 @@ void ExampleClass::pop() {
 
 void ExampleClass::modifyIntegerAtIndex(int index, int newItem) {
 	if (index >= 0 && index < size)
-		vec[index] = newItem;
+		a[index] = newItem;
 	else
 		cout << "Cannot modify the integer at index " << index << " - it is an invalid index." << endl << endl;
 }
@@ -147,17 +142,17 @@ void ExampleClass::modifyIntegerAtIndex(int index, int newItem) {
 
 
 /***** Other Functions*****/
-void ExampleClass::printVec() const {
+void ExampleClass::printArray() const {
 	if (size == 0)
 		cout << "The vector is empty.\n\n";
 	else {
 		cout << "The items in the vector are:\n";
 		for (int i = 0; i < size; i++) {
-			cout << vec[i];
+			cout << a[i];
 			if (i < size - 1)
 				cout << ' ';
 			else
-				cout << endl << endl;
+				cout << endl;
 		}
 	}
 }
@@ -175,5 +170,33 @@ void printStr(const ExampleClass& ec) {
 
 /***** Static Functions *****/
 void ExampleClass::printPI() {
-	cout << "The value of the mathematical constant pi is " << PI << endl << endl;
+	cout << "The value of the mathematical constant pi is " << PI << endl;
+}
+
+
+int ExampleClass::getY() {
+	return y;
+}
+
+
+void ExampleClass::setY(int newY) {
+	y = newY;
+}
+
+
+
+/***** Helper Functions *****/
+void ExampleClass::resizeArray() {
+	cout << "The array is being resized\n";
+
+	/* The array can be resized to any new capacity. A common convention
+	   is to double the capacity. This avoids having to constantly do resize
+	   operations every time a new item is added. */
+	int* newArray = new int[capacity * 2];
+	for (int i = 0; i < capacity; i++)
+		newArray[i] = a[i];
+	
+	delete[] a;
+	a = newArray;
+	capacity *= 2;
 }
