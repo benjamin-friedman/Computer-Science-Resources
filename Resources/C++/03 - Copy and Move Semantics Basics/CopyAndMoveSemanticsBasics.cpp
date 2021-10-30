5d36f131a234cd7be929d80245c9874db5b5e9fe
benjamin-friedman <82235512+benjamin-friedman@users.noreply.github.com>
2021-10-29 23:45:08 -0400

    Update CopyAndMoveSemanticsBasics.cpp

--- Resources/C++/03 - Copy and Move Semantics Basics/CopyAndMoveSemanticsBasics.cpp
+++ Resources/C++/03 - Copy and Move Semantics Basics/CopyAndMoveSemanticsBasics.cpp
@@ -3,6 +3,30 @@
 	- copy assignment/copy constructor
 	- move assignment/move constructor
 	- the "std::move()" function - converts an lvalue to an rvalue, used in move semantics
+	- lvalues vs. rvalues (brief description below)
+
+
+   Though lvalues and rvalues have official definitions, the best way to think of them in general is that an lvalue is
+   something that you can reference by a name, and an rvalue is something that you cannot reference by a name.
+	- lvalues take the form of things like variables and objects
+	- rvalues take the form of things like hardcoded numbers and temporary objects
+   Meaning, in "int x = 3;", x is an lvalue since it's a variable with a name, whereas 3 is an rvalue since it's just
+   the number 3. It is not a variable but rather a temporary value loaded somewhere in the computers memory for the purposes
+   of executing that one line of code.
+   A common misconception about lvalues and rvalues is that the l and r stand for "left" and "right" meaning lvalues go on the
+   left side of the "=" operator, and r values go on the right side. As you will see in the examples below, this is not the case.
+
+	int x = 3;				x is an lvalue, 3 is an rvalue
+	int y = x;				x and y are both lvalues
+	string s = "Liebestraum";		s is an lvalue, "Liebestraum" is an rvalue
+	string s2 = s;				s2 and s are both lvalues
+
+   Can an rvalue go on the left of the "=" operator? Yes.
+	s + s = s;				s + s is an rvalue, s is an lvalue
+   Though this statement has zero utility, it is still valid C++ code.
+
+   There ARE cases where you can refer to an rvalue by a name. This is when you use && to denote something as an rvalue.
+   This is most commonly seen in a formal parameter to a function such as in move semantics as seen below.
 */
 
 #include <iostream>
@@ -13,6 +37,8 @@ class MyClass {
 public:
 	// Default Constructor
 	MyClass();
+	// 1-Parameter Constructor
+	explicit MyClass(int _x);
 
 	// Copy Constructor
 	MyClass(const MyClass& myClass);
@@ -40,7 +66,7 @@ int main() {
 	// CONSTRUCTOR
 	MyClass mc1;
 	MyClass mc2(mc1);		// copy constructor called. mc1 is an lvalue
-
+	MyClass mc22 = mc1;
 	cout << endl << endl;
 
 	MyClass mc3;
@@ -68,12 +94,18 @@ int main() {
 }
 
 
-MyClass::MyClass() {
-	x = 10;
+MyClass::MyClass() : x(10) {
 	a = new int[x];
 	for (int i = 0; i < x; i++)
 		a[i] = i;
 }
+
+explicit MyClass::MyClass(int _x) : x(_x) {
+	a = new int[x];
+	for (int i = 0; i < x; i++)
+		a[i] = i;
+}
+
 
 /* A complete and independent copy of myClass will be made
    and stored in the newly constructed object. */
